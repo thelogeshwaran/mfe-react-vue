1dcf7692e85a8bff46180f6f84f50f58cc7ec66c
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-01-18 18:05:39 +0530

    testing pipeline

--- apps/marketing/src/components/Pricing.js
+++ apps/marketing/src/components/Pricing.js
@@ -1,150 +1,150 @@
 import React from "react";
-// import Button from "@material-ui/core/Button";
-// import Card from "@material-ui/core/Card";
-// import CardActions from "@material-ui/core/CardActions";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import Grid from "@material-ui/core/Grid";
-// import StarIcon from "@material-ui/icons/StarBorder";
-// import Typography from "@material-ui/core/Typography";
-// import Link from "@material-ui/core/Link";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-// import Box from "@material-ui/core/Box";
-// import { Link as RouterLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Grid from "@material-ui/core/Grid";
+import StarIcon from "@material-ui/icons/StarBorder";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import { Link as RouterLink } from "react-router-dom";
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-// const useStyles = makeStyles((theme) => ({
-//   "@global": {
-//     ul: {
-//       margin: 0,
-//       padding: 0,
-//       listStyle: "none",
-//     },
-//   },
-//   toolbar: {
-//     flexWrap: "wrap",
-//   },
-//   toolbarTitle: {
-//     flexGrow: 1,
-//   },
-//   link: {
-//     margin: theme.spacing(1, 1.5),
-//   },
-//   heroContent: {
-//     padding: theme.spacing(8, 0, 6),
-//   },
-//   cardHeader: {
-//     backgroundColor:
-//       theme.palette.type === "light"
-//         ? theme.palette.grey[200]
-//         : theme.palette.grey[700],
-//   },
-//   cardPricing: {
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "baseline",
-//     marginBottom: theme.spacing(2),
-//   },
-//   footer: {
-//     borderTop: `1px solid ${theme.palette.divider}`,
-//     marginTop: theme.spacing(8),
-//     paddingTop: theme.spacing(3),
-//     paddingBottom: theme.spacing(3),
-//     [theme.breakpoints.up("sm")]: {
-//       paddingTop: theme.spacing(6),
-//       paddingBottom: theme.spacing(6),
-//     },
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
 
-// const tiers = [
-//   {
-//     title: "Free",
-//     price: "0",
-//     description: [
-//       "10 users included",
-//       "2 GB of storage",
-//       "Help center access",
-//       "Email support",
-//     ],
-//     buttonText: "Sign up for free",
-//     buttonVariant: "outlined",
-//   },
-//   {
-//     title: "Pro",
-//     subheader: "Most popular",
-//     price: "15",
-//     description: [
-//       "20 users included",
-//       "10 GB of storage",
-//       "Help center access",
-//       "Priority email support",
-//     ],
-//     buttonText: "Get started",
-//     buttonVariant: "contained",
-//   },
-//   {
-//     title: "Enterprise",
-//     price: "30",
-//     description: [
-//       "50 users included",
-//       "30 GB of storage",
-//       "Help center access",
-//       "Phone & email support",
-//     ],
-//     buttonText: "Contact us",
-//     buttonVariant: "outlined",
-//   },
-// ];
-// const footers = [
-//   {
-//     title: "Company",
-//     description: ["Team", "History", "Contact us", "Locations"],
-//   },
-//   {
-//     title: "Features",
-//     description: [
-//       "Cool stuff",
-//       "Random feature",
-//       "Team feature",
-//       "Developer stuff",
-//       "Another one",
-//     ],
-//   },
-//   {
-//     title: "Resources",
-//     description: [
-//       "Resource",
-//       "Resource name",
-//       "Another resource",
-//       "Final resource",
-//     ],
-//   },
-//   {
-//     title: "Legal",
-//     description: ["Privacy policy", "Terms of use"],
-//   },
-// ];
+const tiers = [
+  {
+    title: "Free",
+    price: "0",
+    description: [
+      "10 users included",
+      "2 GB of storage",
+      "Help center access",
+      "Email support",
+    ],
+    buttonText: "Sign up for free",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "Pro",
+    subheader: "Most popular",
+    price: "15",
+    description: [
+      "20 users included",
+      "10 GB of storage",
+      "Help center access",
+      "Priority email support",
+    ],
+    buttonText: "Get started",
+    buttonVariant: "contained",
+  },
+  {
+    title: "Enterprise",
+    price: "30",
+    description: [
+      "50 users included",
+      "30 GB of storage",
+      "Help center access",
+      "Phone & email support",
+    ],
+    buttonText: "Contact us",
+    buttonVariant: "outlined",
+  },
+];
+const footers = [
+  {
+    title: "Company",
+    description: ["Team", "History", "Contact us", "Locations"],
+  },
+  {
+    title: "Features",
+    description: [
+      "Cool stuff",
+      "Random feature",
+      "Team feature",
+      "Developer stuff",
+      "Another one",
+    ],
+  },
+  {
+    title: "Resources",
+    description: [
+      "Resource",
+      "Resource name",
+      "Another resource",
+      "Final resource",
+    ],
+  },
+  {
+    title: "Legal",
+    description: ["Privacy policy", "Terms of use"],
+  },
+];
 
 export default function Pricing() {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <React.Fragment>
-      {/* <Container maxWidth="sm" component="main" className={classes.heroContent}>
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
         <Typography
           component="h1"
           variant="h2"
@@ -173,16 +173,16 @@ export default function Pricing() {
               item
               key={tier.title}
               xs={12}
-              sm={tier.title === 'Enterprise' ? 12 : 6}
+              sm={tier.title === "Enterprise" ? 12 : 6}
               md={4}
             >
               <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  action={tier.title === "Pro" ? <StarIcon /> : null}
                   className={classes.cardHeader}
                 />
                 <CardContent>
@@ -244,7 +244,7 @@ export default function Pricing() {
         <Box mt={5}>
           <Copyright />
         </Box>
-      </Container> */}
+      </Container>
     </React.Fragment>
   );
 }
