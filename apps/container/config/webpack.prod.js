61ccb277794c7aa65f56a353ddb995ede9eba252
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-01-22 14:54:24 +0530

    Update webpack.prod.js

--- apps/container/config/webpack.prod.js
+++ apps/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/late/",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
