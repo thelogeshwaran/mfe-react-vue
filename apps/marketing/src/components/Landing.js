24bead9014310f93bbd58ef751f6112463ce66b0
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-01-24 19:27:35 +0530

    update marketing.yml

--- apps/marketing/src/components/Landing.js
+++ apps/marketing/src/components/Landing.js
@@ -88,7 +88,7 @@ export default function Album() {
             >
               Something short and leading about the collection below—its
               contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirelyyyyy.
+              short so folks don&apos;t simply skip over it entirelyyy.
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justifyContent="center">
