a8828ce71784bb26356783ba7f362d0d75ec04e7
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2024-01-31 16:49:17 +0530

    Update webpack.prod.js

--- apps/container/config/webpack.prod.js
+++ apps/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        marketing: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
